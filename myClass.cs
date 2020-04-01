d669d924376819d6e0328d69c2befc567a3aa2d8
MathiasMeisner <62882868+MathiasMeisner@users.noreply.github.com>
2020-04-01 11:28:07 +0200

    Tilføjet properties + constructor

--- myClass.cs
+++ myClass.cs
@@ -2,6 +2,15 @@ namespace GitExercise
 {
     class myClass
     {
-        
+        private string _name;
+        private string _adress;
+        private int _postnr;
+
+        public myClass(string name, string adress, int postnr)
+        {
+            _name = name;
+            _adress = adress;
+            _postnr = postnr;
+        }
     }
 }
